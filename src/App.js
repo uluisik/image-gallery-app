64d6c0879468c4c5f83a1c0c82e98df8676fefa3
ahmet <ahmet55uluisik05@gmail.com>
2023-05-09 11:06:51 +0300

    improved project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Gallery from "./Gallery";
 
 function App() {
   return (
     <>
       <h1 className="mainH1">Image Gallery</h1>
+      <Gallery />
     </>
   );
 }
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Gallery.css";
+import Images from "./Images";
+import { FaWindowClose } from "react-icons/fa";
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const [model, setModel] = useState(false);
+  const [tempImgSrc, setTempImgSrc] = useState("");
+
+  const getImg = (imgSrc) => {
+    setTempImgSrc(imgSrc);
+    setModel(true);
+  };
+
+  return (
+    <>
+      <div className={model ? "model open" : "model"}>
+        <img src={tempImgSrc} alt="" />
+        <FaWindowClose onClick={() => setModel(false)} className="x" />
+      </div>
+      <div className="gallery">
+        {Images.map((item, index) => {
+          return (
+            <div className="pics" key={index}>
+              <img
+                src={item.image}
+                alt=""
+                style={{ width: "100%" }}
+                onClick={() => getImg(item.image)}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default Gallery;
