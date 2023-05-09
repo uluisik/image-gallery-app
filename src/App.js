75571133aca34b8f79a27a974b4916560e5a54f6
ahmet <ahmet55uluisik05@gmail.com>
2023-05-09 10:32:44 +0300

    added components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <h1 className="mainH1">Image Gallery</h1>
+    </>
+  );
 }
 
 export default App;
